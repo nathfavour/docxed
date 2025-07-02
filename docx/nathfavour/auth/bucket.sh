b5c644c8acf50f0b941f96905efe5567b644efb8
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-07-02 11:57:39 +0100

    🔧 Update Appwrite bucket permissions and add documentation files

--- docx/nathfavour/auth/bucket.sh
+++ docx/nathfavour/auth/bucket.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-APPWRITE_ENDPOINT="YOUR_ENDPOINT_URL"
-APPWRITE_PROJECT_ID="YOUR_PROJECT_ID"
-APPWRITE_API_KEY="YOUR_API_KEY"
+# MY_APPWRITE_ENDPOINT="https://fra.cloud.appwrite.io/v1"
+# MY_APPWRITE_PROJECT_ID="684332040001eee05350"
 
-appwrite client --endpoint "$APPWRITE_ENDPOINT" --project-id "$APPWRITE_PROJECT_ID" --key "$APPWRITE_API_KEY"
+# change variable to my appwrite endpoint. 
+# appwrite client --endpoint "$MY_APPWRITE_ENDPOINT" --project-id "$MY_APPWRITE_PROJECT_ID"
 
 # User Avatars Bucket
 appwrite storage create-bucket \
     --bucket-id "userAvatars" \
     --name "User Avatars" \
-    --permissions "create(\"role:member\")" \
+    --permissions "create(\"users\")" \
     --file-security true \
     --maximum-file-size 2000000 \
     --allowed-file-extensions "jpg" \
@@ -23,7 +23,7 @@ appwrite storage create-bucket \
 appwrite storage create-bucket \
     --bucket-id "encryptedDataBackups" \
     --name "Encrypted Data Backups" \
-    --permissions "create(\"role:member\")" \
+    --permissions "create(\"users\")" \
     --file-security true \
     --maximum-file-size 50000000 \
     --allowed-file-extensions "backup" \
